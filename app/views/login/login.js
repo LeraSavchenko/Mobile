4b8af54f4ad135dd485e1199469c46f85c4d87f1
Lera Savchenko <savchenko.valeriya@gmail.com>
2017-05-22 02:02:45 +0300

    added gallery grid and some other UI elems

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -2,9 +2,16 @@ var frameModule = require("ui/frame");
 var clientId = 'b4b2f6557d644ed88a6b3e53f51a6902';
 var reidrectUri = 'https://github.com/LeraSavchenko/Mobile';
 var secret = 'de7d9ef653e04eb9bf4d86be0c209898';
+var observableModule = require("data/observable");
 
+var user = new observableModule.fromObject({
+    login: 'apoddiachyi',
+    password: 'apoddiachyi1'
+});
 
-exports.loaded = function() {
+exports.loaded = function(args) {
+    page = args.object;
+	page.bindingContext = user;
 	console.log('ASDASDASDASDSADASDAS');
 };
 
@@ -22,10 +29,16 @@ exports.redirect = function() {
 	var image = imageSourceModule.fromFile('~/images/1.jpg');
 	console.log(image);
 	SocialShare.shareImage(image);
-
 };
 
 exports.connect = function() {
-	var topmost = frameModule.topmost	();
-    topmost.navigate("views/main/main");
+	var topmost = frameModule.topmost ();
+    var login = page.getViewById("login");
+	var password = page.getViewById("password");
+	
+	if (login.text && password.text) {
+    	topmost.navigate("views/main/main");
+	} else {
+		alert("Enter correct login params!");
+	}
 };
--- app/views/main/main.js
+++ app/views/main/main.js
@@ -1,3 +1,10 @@
+var frameModule = require("ui/frame");
+
 exports.pageLoaded = function() {
 
+};
+
+exports.logout = function() {
+	var topmost = frameModule.topmost ();
+    topmost.navigate("views/login/login");
 };
