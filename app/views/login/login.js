71e03d7a8af0407e9f6aac3358589ca3e38fbefb
Alex Poddiachyi <poddiachyi@gmail.com>
2017-05-21 22:25:23 +0300

    delete sidedraw

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -3,43 +3,9 @@ var clientId = 'b4b2f6557d644ed88a6b3e53f51a6902';
 var reidrectUri = 'https://github.com/LeraSavchenko/Mobile';
 var secret = 'de7d9ef653e04eb9bf4d86be0c209898';
 
-var TnsSideDrawer = require('nativescript-sidedrawer');
 
 exports.loaded = function() {
 	console.log('ASDASDASDASDSADASDAS');
-
-	console.dir(TnsSideDrawer);
-	TnsSideDrawer.build({
-		templates: [{
-				title: 'Home',
-				androidIcon: 'ic_home_white_24dp',
-				iosIcon: 'ic_home_white',
-		}, {
-				title: 'Judgment Day',
-				androidIcon: 'ic_gavel_white_24dp',
-				iosIcon: 'ic_gavel_white',
-		}, {
-				title: 'Bank Roll',
-				androidIcon: 'ic_account_balance_white_24dp',
-				iosIcon: 'ic_account_balance_white',
-		}, {
-				title: 'Fix Stuff',
-				androidIcon: 'ic_build_white_24dp',
-				iosIcon: 'ic_build_white',
-		}, {
-				title: 'This Is Me',
-				androidIcon: 'ic_account_circle_white_24dp',
-				iosIcon: 'ic_account_circle_white',
-		}],
-		title: 'This App Name',
-		subtitle: 'is just as awesome as this subtitle!',
-		listener: (index) => {
-				this.i = index
-		},
-		context: this,
-	})
-
-	TnsSideDrawer.toggle()
 };
 
 exports.redirect = function() {
